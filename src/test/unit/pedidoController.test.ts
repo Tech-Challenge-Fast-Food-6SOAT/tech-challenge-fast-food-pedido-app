15fa69e1cc5bb7abaa8f6929a62b4317c49e8e36
Danilo Cassola <danilocassola@gmail.com>
2024-11-22 21:07:17 -0300

    test: add PedidoController

---
+++ src/test/unit/pedidoController.test.ts
@@ -0,0 +1,155 @@
+/* eslint-disable @typescript-eslint/init-declarations */
+import { PedidoController } from '@/adapters/controllers';
+import type { CheckoutUseCase, PedidoUseCase } from '@/application/usecases';
+import type { HttpRequest, HttpResponse } from '@/interfaces/http';
+
+describe('PedidoController', () => {
+  const pedidoUseCase = {
+    buscarPedidos: jest.fn(),
+    atualizarStatusPedido: jest.fn(),
+    statusPagamento: jest.fn(),
+    atualizarStatusPagamento: jest.fn(),
+  } as unknown as PedidoUseCase;
+  const checkoutUseCase = {
+    checkout: jest.fn(),
+  } as unknown as CheckoutUseCase;
+  const pedidoController = new PedidoController(pedidoUseCase, checkoutUseCase);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('atualizarStatusPedido', () => {
+    it('should return 200 if status is updated', async () => {
+      const pedido = {
+        id: '123',
+        status: 'Pronto',
+        senha: '0001',
+      };
+      (pedidoUseCase.atualizarStatusPedido as jest.Mock).mockResolvedValue(
+        pedido
+      );
+
+      const request = {
+        body: { status: 'Pronto' },
+        params: { id: '123' },
+      } as unknown as HttpRequest;
+      const response: HttpResponse =
+        await pedidoController.atualizarStatusPedido(request);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.data).toEqual({
+        status: pedido.status,
+        senha: pedido.senha,
+      });
+    });
+
+    it('should return 500 if an error occurs', async () => {
+      (pedidoUseCase.atualizarStatusPedido as jest.Mock).mockRejectedValue(
+        new Error('Erro interno')
+      );
+      const request = {
+        body: { status: 'Pronto' },
+        params: { id: '123' },
+      } as unknown as HttpRequest;
+      const response: HttpResponse =
+        await pedidoController.atualizarStatusPedido(request);
+
+      expect(response.statusCode).toBe(500);
+      expect(response.data).toEqual({ err: 'Erro interno' });
+    });
+  });
+
+  describe('statusPagamento', () => {
+    it('should return 200 and statusPagamento', async () => {
+      const statusPagamento = 'Aprovado';
+      (pedidoUseCase.statusPagamento as jest.Mock).mockResolvedValue(
+        statusPagamento
+      );
+      const request = { params: { id: '123' } } as HttpRequest;
+      const response: HttpResponse = await pedidoController.statusPagamento(
+        request
+      );
+
+      expect(response.statusCode).toBe(200);
+      expect(response.data).toEqual({ statusPagamento });
+    });
+
+    it('should return 500 if an error occurs', async () => {
+      (pedidoUseCase.statusPagamento as jest.Mock).mockRejectedValue(
+        new Error('Erro interno')
+      );
+      const request = { params: { id: '123' } } as HttpRequest;
+      const response: HttpResponse = await pedidoController.statusPagamento(
+        request
+      );
+
+      expect(response.statusCode).toBe(500);
+      expect(response.data).toEqual({ err: 'Erro interno' });
+    });
+  });
+
+  describe('atualizarStatusPagamento', () => {
+    it('should return 200 if statusPagamento is updated', async () => {
+      const message = 'Status do pagamento atualizado com sucesso!';
+      (pedidoUseCase.atualizarStatusPagamento as jest.Mock).mockResolvedValue(
+        message
+      );
+
+      const request = {
+        body: { pagamentoStatus: 'Aprovado' },
+        params: { id: '123' },
+      } as unknown as HttpRequest;
+      const response: HttpResponse =
+        await pedidoController.atualizarStatusPagamento(request);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.data).toEqual({ message });
+    });
+
+    it('should return 500 if an error occurs', async () => {
+      (pedidoUseCase.atualizarStatusPagamento as jest.Mock).mockRejectedValue(
+        new Error('Erro interno')
+      );
+      const request = {
+        body: { pagamentoStatus: 'Aprovado' },
+        params: { id: '123' },
+      } as unknown as HttpRequest;
+      const response: HttpResponse =
+        await pedidoController.atualizarStatusPagamento(request);
+
+      expect(response.statusCode).toBe(500);
+      expect(response.data).toEqual({ err: 'Erro interno' });
+    });
+  });
+
+  describe('checkout', () => {
+    it('should return 200 and data if checkout is successful', async () => {
+      const data = { id: '321', senha: '0001', qrCode: 'qrCode' };
+      (checkoutUseCase.checkout as jest.Mock).mockResolvedValue(data);
+
+      const request = {
+        body: { produtos: [{ id: '123', quantidade: 2 }] },
+        headers: { cpf: '12345678900' },
+      } as HttpRequest;
+      const response: HttpResponse = await pedidoController.checkout(request);
+
+      expect(response.statusCode).toBe(201);
+      expect(response.data).toEqual(data);
+    });
+
+    it('should return 500 if an error occurs', async () => {
+      (checkoutUseCase.checkout as jest.Mock).mockRejectedValue(
+        new Error('Erro interno')
+      );
+      const request = {
+        body: { produtos: [{ id: '123', quantidade: 2 }] },
+        headers: { cpf: '12345678900' },
+      } as HttpRequest;
+      const response: HttpResponse = await pedidoController.checkout(request);
+
+      expect(response.statusCode).toBe(500);
+      expect(response.data).toEqual({ err: 'Erro interno' });
+    });
+  });
+});
